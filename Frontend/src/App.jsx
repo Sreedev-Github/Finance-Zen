00c642974f3e5241a73cd2c7bbb877d3194768d6
Sreedev-Github <sreedev0612@gmail.com>
2024-06-29 13:42:46 +0530

    Some Changes for deployment

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import {Header} from './components/index'
-import {Footer} from "./components/index"
-import { AuthProvider } from './utils/AuthProvider.jsx';
+import React from "react";
+import { Routes, Route, Outlet } from "react-router-dom";
+import { Header, Footer } from "./components/index";
+import Home from './pages/Home';
+import User from './pages/User';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import AddTransaction from './pages/AddTransaction';
+import EditTransaction from './pages/EditTransaction';
+import ProtectedRoute from './utils/ProtectedRoute';
 
 function App() {
-
   return (
-    <AuthProvider>
     <main>
-      <Header/>
-        <Outlet />
-        <Footer/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/user"
+          element={
+            <ProtectedRoute>
+              <User />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-finance"
+          element={
+            <ProtectedRoute>
+              <AddTransaction />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit/:transactionType/:transactionId"
+          element={
+            <ProtectedRoute>
+              <EditTransaction />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+      <Outlet />
+      <Footer />
     </main>
-    </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,62 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store.js';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
-import User from './pages/User';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import AddTransaction from './pages/AddTransaction';
-import EditTransaction from './pages/EditTransaction';
-import ProtectedRoute from './utils/ProtectedRoute';
+import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './utils/AuthProvider.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { path: '/', element: <Home /> },
-      { path: '/signup', element: <Signup /> },
-      { path: '/login', element: <Login /> },
-      {
-        path: '/user',
-        element: (
-          <ProtectedRoute>
-            <User />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: '/add-finance',
-        element: (
-          <ProtectedRoute>
-            <AddTransaction />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: '/edit/:transactionType/:transactionId',
-        element: (
-          <ProtectedRoute>
-            <EditTransaction />
-          </ProtectedRoute>
-        ),
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <AuthProvider>
-        <RouterProvider router={router}>
+        <BrowserRouter>
           <App />
-        </RouterProvider>
+        </BrowserRouter>
       </AuthProvider>
     </Provider>
   </React.StrictMode>
